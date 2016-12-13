d52c693d61065ab8e56028f7a9c8ba92dd41118e
Fabio Duran Verdugo <fabioduran@gnome.org>
2016-12-13 16:37:13 -0300

    Se crea directorio parametros en templates y a fin de estructurar templates por aplicacion, además se modifican ficheros templates involucrados. Se elimina templates no usados

--- parametros/views.py
+++ parametros/views.py
@@ -99,14 +99,14 @@ class ViewDeleteView(SuccessMessageMixin, DeleteView):
 
 class CarreraView(ViewListView):
     model = Carrera
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Carreras"
     extra_context = {}
 
 
 class CarreraCreateView(ViewCreateView):
     form_class = CarreraForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Carrera"
     success_message = "La carrera %(nombre)s ha sido creado"
     success_url = "/carrera/"
@@ -115,28 +115,28 @@ class CarreraCreateView(ViewCreateView):
 class CarreraUpdateView(ViewUpdateView):
     model = Carrera
     form_class = CarreraForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "La carrera %(name)s ha sido actualizado"
     sucess_url = "/carrera/"
 
 
 class CarreraDeleteView(ViewDeleteView):
     model = Carrera
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'La carrera %(nombre)s ha sido Eliminada'
     sucess_url = "/carrera/"
 
 
 class PlanView(ViewListView):
     model = Plan
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Planes"
     extra_context = {}
 
 
 class PlanCreateView(ViewCreateView):
     form_class = PlanForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Plan"
     success_message = "El Plan %(nombre)s ha sido creado"
     success_url = "/plan/"
@@ -145,28 +145,28 @@ class PlanCreateView(ViewCreateView):
 class PlanUpdateView(ViewUpdateView):
     model = Plan
     form_class = PlanForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "El Plan %(name)s ha sido actualizado"
     sucess_url = "/plan/"
 
 
 class PlanDeleteView(ViewDeleteView):
     model = Plan
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'El Plan %(nombre)s ha sido Eliminado'
     sucess_url = "/plan/"
 
 
 class ModuloView(ViewListView):
     model = Modulo
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Modulos"
     extra_context = {}
 
 
 class ModuloCreateView(ViewCreateView):
     form_class = ModuloForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Modulo"
     success_message = "El Modulo %(nombre)s ha sido creado"
     success_url = "/modulo/"
@@ -175,29 +175,28 @@ class ModuloCreateView(ViewCreateView):
 class ModuloUpdateView(ViewUpdateView):
     model = Departamento
     form_class = DepartamentoForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "El Modulo %(name)s ha sido actualizado"
     sucess_url = "/modulo/"
 
 
 class ModuloDeleteView(ViewDeleteView):
     model = Modulo
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'El Modulo %(nombre)s ha sido Eliminado'
     sucess_url = "/modulo/"
 
 
-
 class DepartamentoView(ViewListView):
     model = Departamento
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Departamentos"
     extra_context = {}
 
 
 class DepartamentoCreateView(ViewCreateView):
     form_class = DepartamentoForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Departamento"
     success_message = "El Departamento %(nombre)s ha sido creado"
     success_url = "/departamento"
@@ -206,27 +205,27 @@ class DepartamentoCreateView(ViewCreateView):
 class DepartamentoUpdateView(ViewUpdateView):
     model = Departamento
     form_class = DepartamentoForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "El Departamento %(name)s ha sido actualizado"
     sucess_url = "/departamento"
 
 
 class DepartamentoDeleteView(ViewDeleteView):
     model = Departamento
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'El Departamento %(nombre)s ha sido Eliminado'
     sucess_url = "/departamento"
 
 class AnioView(ViewListView):
     model = Anio
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Años"
     extra_context = {}
 
 
 class AnioCreateView(ViewCreateView):
     form_class = AnioForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Año"
     success_message = "El Año %(nombre)s ha sido creado"
     success_url = "/anio"
@@ -235,28 +234,28 @@ class AnioCreateView(ViewCreateView):
 class AnioUpdateView(ViewUpdateView):
     model = Anio
     form_class = AnioForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "El Año %(name)s ha sido actualizado"
     sucess_url = "/anio"
 
 
 class AnioDeleteView(ViewDeleteView):
     model = Anio
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'El Año %(nombre)s ha sido Eliminado'
     sucess_url = "/anio"
 
 
 class PeriodoView(ViewListView):
     model = Periodo
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Periodos"
     extra_context = {}
 
 
 class PeriodoCreateView(ViewCreateView):
     form_class = PeriodoForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Periodo"
     success_message = "El Periodo %(nombre)s ha sido creado"
     success_url = "/periodo"
@@ -265,28 +264,28 @@ class PeriodoCreateView(ViewCreateView):
 class PeriodoUpdateView(ViewUpdateView):
     model = Periodo
     form_class = PeriodoForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "El Periodo %(name)s ha sido actualizado"
     sucess_url = "/periodo"
 
 
 class PeriodoDeleteView(ViewDeleteView):
     model = Periodo
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'El Periodo %(nombre)s ha sido Eliminado'
     sucess_url = "/periodo"
 
 
 class BloqueView(ViewListView):
     model = Bloque
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Bloques"
     extra_context = {}
 
 
 class BloqueCreateView(ViewCreateView):
     form_class = BloqueForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     titulo = "Agrega Bloques"
     success_message = "El Bloque %(nombre)s ha sido creado"
     success_url = "/bloque"
@@ -295,21 +294,21 @@ class BloqueCreateView(ViewCreateView):
 class BloqueUpdateView(ViewUpdateView):
     model = Bloque
     form_class = BloqueForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "El Bloque %(name)s ha sido actualizado"
     sucess_url = "/bloque"
 
 
 class BloqueDeleteView(ViewDeleteView):
     model = Bloque
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'Bloque %(nombre)s ha sido Eliminado'
     sucess_url = "/bloque"
 
 
 class ProfesorView(ViewListView):
     model = Profesor
-    template_name = "maestro.html"
+    template_name = "parametros/maestro.html"
     titulo = "Profesores"
     extra_context = {}
 
@@ -319,10 +318,9 @@ class ProfesorView(ViewListView):
         return context
 
 
-
 class ProfesorCreateView(ViewCreateView):
     form_class = ProfesorForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = 'Profesor %(nombre)s ha sido creado'
     titulo = 'Agregar Profesor'
     success_url = '/profesor/'
@@ -331,7 +329,7 @@ class ProfesorCreateView(ViewCreateView):
 class ProfesorUpdateView(ViewUpdateView):
     model = Profesor
     form_class = ProfesorForm
-    template_name = "form.html"
+    template_name = "parametros/form.html"
     success_message = "Profesor %(nombre)s ha sido actualizado"
     success_url = "/profesor/"
 
@@ -339,7 +337,7 @@ class ProfesorUpdateView(ViewUpdateView):
 
 class ProfesorDeleteView(ViewDeleteView):
     model = Profesor
-    template_name = "elimina.html"
+    template_name = "parametros/elimina.html"
     success_message = 'Profesor %(nombre)s ha sido Eliminado'
     sucess_url = "/profesor/"
 
