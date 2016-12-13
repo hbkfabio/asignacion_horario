01812748b1490d55767d133c9420041728b8f79e
Fabio Duran Verdugo <fabioduran@gnome.org>
2016-12-13 16:04:37 -0300

    rename

--- asignacion_horario/wsgi.py
+++ asignacion_horario/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "horario.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "asignacion_horario.settings")
 
 application = get_wsgi_application()
