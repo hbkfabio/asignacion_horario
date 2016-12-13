e4aeff77f043dc29bd40a8fef3561395a2a7a4d0
Fabio Duran Verdugo <fabioduran@gnome.org>
2016-12-13 13:35:14 -0300

    Se corrige urls para update de Modelos

--- carrera/urls.py
+++ carrera/urls.py
@@ -29,16 +29,15 @@ urlpatterns = [
     url(r'^carrera/$', CarreraView, name='Carrera'),
     url(r'^plan/$', PlanView, name='Plan'),
     url(r'^modulo/$', ModuloView, name='Modulo'),
-    # url(r'^anio/$', AnioView, name='Anio'),
     #departamento
     url(r'^departamento/$', DepartamentoView.as_view(), name='Departamento'),
     url(r'^departamento/add/$', DepartamentoCreateView.as_view(), name='Departamento_add'),
-    url(r'^departamento/?P<pk>\d+/$', DepartamentoUpdateView.as_view(), name='Departamento_update'),
+    url(r'^departamento/(?P<pk>\d+)/$', DepartamentoUpdateView.as_view(), name='Departamento_update'),
     url(r'^departamento/(?P<pk>\d+)/delete/$', DepartamentoDeleteView.as_view(), name='Departamento_delete'),
     #año
     url(r'^anio/$', AnioView.as_view(), name='Anio'),
     url(r'^anio/add/$', AnioCreateView.as_view(), name='Anio_add'),
-    url(r'^anio/?P<pk>\d+/$', AnioUpdateView.as_view(), name='Anio_update'),
+    url(r'^anio/(?P<pk>\d+)/$', AnioUpdateView.as_view(), name='Anio_update'),
     url(r'^anio/(?P<pk>\d+)/delete/$', AnioDeleteView.as_view(), name='Anio_delete'),
     #Periodo
     url(r'^periodo/$', PeriodoView.as_view(), name='Periodo'),
@@ -48,7 +47,7 @@ urlpatterns = [
     #bloque
     url(r'^bloque/$', BloqueView.as_view(), name='Bloque'),
     url(r'^bloque/add/$', BloqueCreateView.as_view(), name='Bloque_add'),
-    url(r'^bloque/?P<pk>\d+/$', BloqueUpdateView.as_view(), name='Bloque_update'),
+    url(r'^bloque/(?P<pk>\d+)/$', BloqueUpdateView.as_view(), name='Bloque_update'),
     url(r'^bloque/(?P<pk>\d+)/delete/$', ProfesorDeleteView.as_view(), name='Bloque_delete'),
     #profesor
     url(r'^profesor/$', ProfesorView.as_view(), name='Profesor'),
