4a6566177472918857b80edcd6ed3c4972c9efc6
Fabio Duran Verdugo <fabioduran@gnome.org>
2016-12-14 17:29:35 -0300

    Se agrega boton anadir para cada template del modelo y vista

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,15 +1,29 @@
 $(document).on("click", ".edit", function(event){
 
-    var title = $(this).closest("tr").find("th").text();
     var pathname = window.location.pathname;
-    console.log(pathname+title);
-    window.location.href = pathname+title;
-
+    var id = $(this).val();
+    window.location.href = pathname+"edit/"+id;
 });
+
+
+$(document).on("click", ".delete", function(event){
+
+    var pathname = window.location.pathname;
+    var id = $(this).val();
+    window.location.href = pathname+id+"/delete/";
+});
+
+
+$(document).on("click", ".add", function(event){
+
+    var pathname = window.location.pathname;
+    var id = $(this).val();
+    window.location.href = pathname+"add/";
+});
+
 
 $(document).on("click", "button[type='reset']", function(){
     var pathname = window.location.pathname.split("/")[1];
-    console.log(pathname);
     pathname="/"+pathname+"/";
     window.location.href =pathname;
 
