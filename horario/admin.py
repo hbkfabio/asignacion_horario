e6b5ef42f5d67894c99aea85c496635c0e6390f1
Fabio Duran Verdugo <fabioduran@gnome.org>
2016-12-21 17:58:27 -0300

    se crea modelo PeriodoProfesorModelo para app horario, esta clase permite asociar a profesores a modulos para un periodo determinado

--- horario/admin.py
+++ horario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class PeriodoProfesorModuloAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PeriodoProfesorModulo, PeriodoProfesorModuloAdmin)
--- horario/models.py
+++ horario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from parametros.models import Periodo, Profesor, Modulo
 # Create your models here.
+
+class PeriodoProfesorModulo(models.Model):
+    periodo = models.ForeignKey(Periodo)
+    profesor = models.ForeignKey(Profesor)
+    modulo = models.ForeignKey(Modulo)
+
+    def __str__(self):
+        return ('%s %s %s')%(self.periodo, self.profesor, self.modulo)
