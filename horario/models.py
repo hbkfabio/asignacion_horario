59948ddea05c1858a87db3db0742d04c61849972
Fabio Duran Verdugo <fabioduran@gnome.org>
2017-03-02 17:03:50 -0300

    Se crea en parametros la clase que referencia a las actividades que se pueden realizar dentro de un horario

--- horario/models.py
+++ horario/models.py
@@ -15,26 +15,26 @@ class PeriodoProfesorModulo(models.Model):
         return ('%s %s %s')%(self.periodo, self.profesor, self.modulo)
 
 
-# class Horario(models.Model):
-#     periodoprofesormodulo = models.ForeignKey(PeriodoProfesorModulo)
-#     bloque1 = models.CharField(max_length=1)
-#     bloque2 = models.CharField(max_length=1)
-#     bloque3 = models.CharField(max_length=1)
-#     bloque4 = models.CharField(max_length=1)
-#     bloque5 = models.CharField(max_length=1)
-#     bloque6 = models.CharField(max_length=1)
-#     bloque7 = models.CharField(max_length=1)
-#     bloque8 = models.CharField(max_length=1)
-#     bloque9 = models.CharField(max_length=1)
-#     bloque10 = models.CharField(max_length=1)
-#     dia_semana = models.CharField(max_length=20, default="")
-
-
-
 class Horario(models.Model):
     periodoprofesormodulo = models.ForeignKey(PeriodoProfesorModulo)
-    bloque = models.ForeignKey(Bloque)
+    bloque1 = models.CharField(max_length=1)
+    bloque2 = models.CharField(max_length=1)
+    bloque3 = models.CharField(max_length=1)
+    bloque4 = models.CharField(max_length=1)
+    bloque5 = models.CharField(max_length=1)
+    bloque6 = models.CharField(max_length=1)
+    bloque7 = models.CharField(max_length=1)
+    bloque8 = models.CharField(max_length=1)
+    bloque9 = models.CharField(max_length=1)
+    bloque10 = models.CharField(max_length=1)
     dia_semana = models.CharField(max_length=20, default="")
+
+
+
+# class Horario(models.Model):
+#     periodoprofesormodulo = models.ForeignKey(PeriodoProfesorModulo)
+#     bloque = models.ForeignKey(Bloque)
+#     dia_semana = models.CharField(max_length=20, default="")
 
 
 
--- parametros/models.py
+++ parametros/models.py
@@ -87,3 +87,10 @@ class Bloque(models.Model):
 
     def __str__(self):
         return self.nombre
+
+
+class Actividad(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
