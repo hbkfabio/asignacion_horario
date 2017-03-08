07bbddb70809f7d2590fbcec17ad8e8f87a77231
Fabio Duran Verdugo <fabioduran@gnome.org>
2017-03-08 10:43:32 -0300

    Se crean validaciones en forms de parametros para las clases de Actividades, Semestre, Profesor, Bloque, Periodo, se crea una funcion especial para validar los campos de nombre, debido a que todas las clases contienen el atributo (campo) en un intento de no repetir código

--- parametros/forms.py
+++ parametros/forms.py
@@ -19,15 +19,16 @@ import inspect
 
 
 def retrieve_name(var):
-        """
-        Gets the name of var. Does it from the out most frame inner-wards.
-        :param var: variable to get name from.
-        :return: string
-        """
-        for fi in reversed(inspect.stack()):
-            names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
-            if len(names) > 0:
-                return names[0]
+    """
+    Gets the name of var. Does it from the out most frame inner-wards.
+    :param var: variable to get name from.
+    :return: string
+    """
+
+    for fi in reversed(inspect.stack()):
+        names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
+        if len(names) > 0:
+            return names[0]
 
 def valida_campo(modelo, campo, valor):
     varname_campo = retrieve_name(campo)
@@ -40,11 +41,41 @@ def valida_campo(modelo, campo, valor):
         return n
 
 
+def cleaned_nombre(self, modelo):
+    """
+    Función que valida para todos los formularios el campo nombre.
+
+    Devuelve limpio de caracteres "especiales" los datos insertos en los
+    diccionario de los formularios.
+
+    This method returns the clean data, which is then inserted into the
+    cleaned_data dictionary of the form.
+
+    Valida si el valor del campo nombre existe en la base de datos.
+
+    Validate if row value nombre exists in data base.
+
+    param self: object value from form class
+    param modelo: model object from model class
+    return: string with data cleaned or raise message error
+    """
+
+    n = self.cleaned_data.get("nombre")
+    pk = self.instance.pk
+    query = modelo.objects.all().filter(nombre = n)
+    query = query.filter(~Q(id=pk))
+    if query.exists():
+        raise forms.ValidationError("%s ya se ha ingresado"%n)
+    return n
+
+
 class MaestroForm(forms.ModelForm):
 
     class Meta:
         model = None
         fields = ["nombre", "descripcion"]
+
+
 
 
 class DepartamentoForm(MaestroForm):
@@ -94,11 +125,32 @@ class PeriodoForm(forms.ModelForm):
         fields = ["nombre", "anio"]
 
 
+    def clean_anio(self):
+        n = self.cleaned_data.get("nombre")
+        a = self.cleaned_data.get("anio")
+        pk = self.instance.pk
+        query = Periodo.objects.all().filter(anio = a)
+        query = query.filter(nombre = n)
+        query = query.filter(~Q(id=pk))
+        if query.exists():
+            raise forms.ValidationError(
+                "%s ya se ha ingresado para el año %s "%(n, a)
+                )
+        return a
+
+
 class BloqueForm(forms.ModelForm):
 
     class Meta:
         model = Bloque
         fields = ["nombre"]
+
+
+    def clean_nombre(self):
+        n = cleaned_nombre(self, Bloque)
+
+        return n
+
 
 
 class ProfesorForm(forms.ModelForm):
@@ -108,6 +160,17 @@ class ProfesorForm(forms.ModelForm):
         fields = ["nombre", "rut", "correo", "departamento"]
 
 
+    def clean_nombre(self):
+        n = cleaned_nombre(self, Profesor)
+
+        return n
+
+    def clean_correo(self):
+        n = self.cleaned_data.get("correo")
+
+        return n
+
+
 
 class SemestreForm(forms.ModelForm):
 
@@ -115,6 +178,10 @@ class SemestreForm(forms.ModelForm):
         model = Semestre
         fields = ["nombre"]
 
+    def clean_nombre(self):
+        n = cleaned_nombre(self, Semestre)
+
+        return n
 
 
 class ActividadForm(forms.ModelForm):
@@ -125,12 +192,6 @@ class ActividadForm(forms.ModelForm):
                 "identificador",]
 
     def clean_nombre(self):
-        n = self.cleaned_data.get("nombre")
-        # n = cleaned_data("nombre")
-        pk = self.instance.pk
-        print (pk)
-        query = Actividad.objects.all().filter(nombre = n)
-        query = query.filter(~Q(id=pk))
-        if query.exists():
-            raise forms.ValidationError("%s ya se ha ingresado"%n)
+        n = cleaned_nombre(self, Actividad)
+
         return n
