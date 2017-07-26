9cb921faa16471401be2365f989528936095d759
Fabio Duran Verdugo <fabioduran@gnome.org>
2017-07-26 18:01:49 -0400

    Se limpia codigo y se extraen funciones duplicadas

--- static/js/change-horario.js
+++ static/js/change-horario.js
@@ -10,48 +10,19 @@ $(document).on("click", ".change", function(event){
   var col = $(this).parent().children().index($(this));
   var row = $(this).parent().parent().children().index($(this).parent());
 
-    valor = celda.text();
+  valor = celda.text();
+  alert("valor: " + valor);
   create_combo_selected(celda);
   celda.attr("class","");
-  // var dia_semana = $(this).find('td:first');
-  // var dia_semana = $(table).find('td:first').text() //
-  // console.log(dia_semana);
-
-  
-
-  // var dia_semana = $(this).parents('tr:first').find('td:first').text();
-
-  // console.log(dia_semana);
-
-  //indice de columnas
-  
-  console.log('Row: ' + row + ', Column: ' + col + ' Valor: '+ valor);
-
-  
-  create_combo_selected(celda);
-  celda.attr("class","");
-  
-
- // if(valor == " "){
-
-//    celda.text("X");
-
-//  }else{
-
- //   celda.text(" ");
-
- // }
 
 });
 
-function save_combo_selected(celda, row, colum){
-}
 
 function create_combo_selected(celda){
   var valor = celda.text();
-  celda.text("");
+  alert(valor);
 
-  //console.log('dato: '+valor);
+  celda.text("");
 
   var url = '/horario/param/get_actividad/';
   var diccionario = {};
@@ -69,34 +40,6 @@ function create_combo_selected(celda){
         });
 }
 
-function load_select_actividad(widget, data, valor){
-
-  var name = widget.attr("name");
-  var txt = 'Seleccione un(a)  ${name}';
-  var html = "";
-
-    html +='<select class="combo-option">';
-    html+= ('<option value=" "></option>');
-
-  $.each(data, function(i, val){
-    txt = val["fields"]["identificador"]
-
-   if(valor == txt){
-
-      html+= ('<option value='+txt+' selected>'+txt+'</option>');
-
-   }
-    else{
-
-      html+= ('<option value='+txt+'>'+txt+'</option>');
-    
-     }
-    
-    });
-  html += '</select>';
-
-  widget.append(html);
-}
 
 $(document).on("change", ".combo-option", function(event){
 
@@ -105,9 +48,7 @@ $(document).on("change", ".combo-option", function(event){
   var combo = $(this);
   var valor = combo.val();
 
-  console.log(combo);
-
-  td = $(this).parent();
+  td = combo.parent();
   td.html(valor);
   td.attr('class', 'change');
 
@@ -156,7 +97,7 @@ $(document).on("change", ".combo-option", function(event){
      url: "/horario/save/",
      data: param,
 
-     
+
   })
     .done(function(data){
       //console.log(data);
@@ -170,38 +111,6 @@ console.log('datos enviados');
 
 });
 
-function create_combo_selected(celda){
-  var valor = celda.text();
-  celda.text("");
-console.log(valor);
-
-  var url = '/horario/param/get_actividad/';
-  var diccionario = {};
- console.log("HOLA1");
-
-   $.ajax({
-         type: "POST",
-         url: url,
-         data: diccionario,
-         async: false,
-        })
-        .done(function(data){
-          data = $.parseJSON(data);
-          load_select_actividad(celda, data, valor)
-        });
-
-//html ='<select class="combo-option">';
- // html += '<option value=""> </option>';
- // html += '<option value="C" selected>C</option>';
- // html += '<option value="A">A</option>';
- // html += '<option value="L">L</option>';
- // html += '<option value="S">S</option>';
- // html += '</select>';
-  
- // celda.append(html);
-  
-
-}
 
 function load_select_actividad(widget, data, valor){
 
@@ -209,18 +118,15 @@ function load_select_actividad(widget, data, valor){
   var txt = 'Seleccione un(a)  ${name}';
   var html = "";
 
-  console.log("HOLA");
-
-  //$(widget).append(
-  //    $('<option></option>').val("").html(txt)
- //   );
-    html +='<select class="combo-option">';
-    html+= ('<option value=" "></option>');
+  html +='<select class="combo-option">';
+  html+= ('<option value=" "></option>');
 
   $.each(data, function(i, val){
     //console.log(val);
     //pk = val["pk"]
     txt = val["fields"]["identificador"]
+    console.log("txt: " + txt)
+    console.log("valor: " + valor)
 
    if(valor == txt){
 
@@ -230,31 +136,10 @@ function load_select_actividad(widget, data, valor){
     else{
     html+= ('<option value='+txt+'>'+txt+'</option>');
     }
-    
+
     });
   html += '</select>';
 
   widget.append(html);
 
-
 }
-
-$(document).on("change", ".combo-option", function(event){
-
-  event.preventDefault();
-
-  var combo = $(this);
-  var valor = combo.val();
-
-  console.log(combo);
-
-  td = $(this).parent();
-  td.html(valor);
-  td.attr('class', 'change');
-
-  titulo_bloque = td.closest('table').find('th').eq(td.index());
-  console.log(titulo_bloque.text());
-
-  event.stopPropagation();
-
-});
